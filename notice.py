ca9f87d9fc8e731a6c50ca3589becc53ddb35129
fanyuanqi <fanyq15@mails.tsinghua.edu.cn>
2017-08-30 23:01:29 +0800

    Update notice.py

--- notice.py
+++ notice.py
@@ -11,13 +11,13 @@ import time
 offs=1
 stop=1
 while stop==1:
-  name='fanyq15' ###学堂账号
-  mima='Fan179175'###学堂密码
-  mail='1004039684@qq.com'###接受邮箱（必须将发送邮箱设置为白名单）
+  name='your_root' ###学堂账号
+  mima='your_mima'###学堂密码
+  mail='*************@qq.com'###接受邮箱（必须将发送邮箱设置为白名单）
   login(name,mima)
   semester = Semester(current=True)
   if offs==1:
-    db = pymysql.connect("localhost","root","aa701022","system" )###数据库密码，账号，地址
+    db = pymysql.connect("localhost","root","*********","system" )###数据库密码，账号，地址
     cursor = db.cursor()
     sql = "INSERT INTO people(name,mima,mail) VALUES ('%s', '%s', '%s')" %(name,mima,mail)
     try:
@@ -55,7 +55,7 @@ while stop==1:
               d=d.replace("’","")
               d=d.replace("”","")
               d=d.replace("“","")
-              db = pymysql.connect("localhost","root","aa701022","system",charset="utf8")###数据库密码，账号，地址
+              db = pymysql.connect("localhost","root","***********","system",charset="utf8")###数据库密码，账号，地址
               cursor = db.cursor()
               sql2 = "INSERT INTO notice(name,title,class,contant) VALUES ('%s', '%s', '%s', '%s');" %(name,message.title,course.name,d)
               try:
@@ -90,7 +90,7 @@ while stop==1:
               d=d.replace("’","")
               d=d.replace("”","")
               d=d.replace("“","")
-              db = pymysql.connect("localhost","root","aa701022","system",charset="utf8")###数据库密码，账号，地址
+              db = pymysql.connect("localhost","root","*********","system",charset="utf8")###数据库密码，账号，地址
               cursor = db.cursor()
               sql3 = "SELECT * FROM notice  WHERE name='%s' AND class='%s' AND title='%s' AND contant='%s';" %(name,course.name,message.title,d)
               try:
@@ -101,15 +101,15 @@ while stop==1:
                    db.close()
                    del cursor
                    del db 
-                   db = pymysql.connect("localhost","root","aa701022","system",charset="utf8")###数据库密码，账号，地址
+                   db = pymysql.connect("localhost","root","***********","system",charset="utf8")###数据库密码，账号，地址
                    cursor = db.cursor()
                    localtime = time.asctime( time.localtime(time.time()) )
                    sql2 = "INSERT INTO notice(name,title,class,contant) VALUES ('%s', '%s', '%s', '%s');" %(name,message.title,course.name,d)
                    mail_in="%s，你好，这里有新的课程通知：\n\n课程名：%s\n\n通知标题：%s\n\n通知内容：%s\n\n\n\n%s"%(name,course.name,message.title,d,localtime)
                    msg = MIMEText(mail_in,'plain','utf-8')
-                   from_addr = 'THU_notice@163.com'      ###发送邮件邮箱
-                   password = 'aa701022'                 ###发送邮件密码
-                   to_addr = '1004039684@qq.com'         ###接受邮箱
+                   from_addr = '*********'      ###发送邮件邮箱
+                   password = '*********'                 ###发送邮件密码
+                   to_addr = '**********'         ###接受邮箱
                    smtp_server = 'smtp.163.com'          ###smtp服务器地址
                    msg['From'] = from_addr
                    msg['To'] = to_addr
